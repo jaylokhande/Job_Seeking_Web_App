d5a431e87b3142f95cdb70916c51939913c8ce17
jay <jaylokhande5727@gmail.com>
2024-03-25 18:46:57 +0530

    logout functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,3 +48,13 @@ export const login = catchAsyncError(async(req,res,next)=>{
     }
     sendToken(user,200,res,"User Logged in Successfully")
 })
+
+export const logout = catchAsyncError(async(req,res,next)=>{
+    res.status(201).cookie("token","",{
+        httpOnly: true ,
+        expires : new Date(Date.now())
+    }).json({
+        success : true ,
+        message : "User Logged Out"
+    });
+});
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {login, register} from '../controllers/userController.js'
+import {login, register,logout} from '../controllers/userController.js'
 
 const router = express.Router();
 
  router.post("/register",register);
- router.post("/login",login)
+ router.post("/login",login);
+ router.get("/logout",logout);
 export default router ;
