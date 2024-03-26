3b7b74352ba86971aa18341c0d96512b7e0cfc7c
jay <jaylokhande5727@gmail.com>
2024-03-26 22:34:03 +0530

    router comments

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,7 +4,7 @@ import {login, register,logout} from '../controllers/userController.js'
 import { isAuthorized } from "../middlewares/auth.js";
 
 const router = express.Router();
-
+// all router here
  router.post("/register",register);
  router.post("/login",login);
  router.get("/logout",isAuthorized,logout);
