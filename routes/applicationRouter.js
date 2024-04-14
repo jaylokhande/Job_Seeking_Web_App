87b34686fdfc2dbdede9e5b4f0e1b55e5655b697
jay <jaylokhande5727@gmail.com>
2024-04-14 18:46:06 +0530

    application router added

--- routes/applicationRouter.js
+++ routes/applicationRouter.js
@@ -1,5 +1,16 @@
 import express from "express";
+import {
+  employerGetAllApplications,
+  jobseekerDeleteApplication,
+  jobseekerGetAllApplications,
+} from "../controllers/applicationController.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-export default router ;
+// router.post("/post", isAuthenticated, postApplication);
+router.get("/employer/getall", isAuthenticated, employerGetAllApplications);
+router.get("/jobseeker/getall", isAuthenticated, jobseekerGetAllApplications);
+router.delete("/delete/:id", isAuthenticated, jobseekerDeleteApplication);
+
+export default router;
